4d6ed646217613803300b71ca7a1ca9f453f6378
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-02 20:33:14 -0300

    exercicio1-typescript

---
+++ exercicio1.ts
@@ -0,0 +1,16 @@
+let nota1: number = 3
+let nota2: number = 7
+let nota3: number = 5
+
+let media: number = (nota1 + nota2 + nota3) / 3
+
+console.log("A média é " + media)
+
+if(media >= 7) {
+    console.log("APROVADO")
+
+} else {
+
+    console.log("REPROVADO")
+}
+

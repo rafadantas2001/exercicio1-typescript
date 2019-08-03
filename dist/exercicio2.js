b3ffe12d3262593604c1cbe333b7819789eb3adc
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-02 21:49:20 -0300

    exercicio2-typescript

---
+++ dist/exercicio2.js
@@ -0,0 +1,10 @@
+var participantes = 10;
+var folhas = 100;
+var folhasporparticipantes = 10;
+var multiplicacao = (participantes * folhasporparticipantes);
+if (multiplicacao > folhas) {
+    console.log("Não");
+}
+else {
+    console.log("Sim");
+}
---
+++ exercicio2.ts
@@ -0,0 +1,17 @@
+let participantes: number = 10
+let folhas: number = 100
+let folhasporparticipantes: number = 10
+
+let multiplicacao: number = (participantes*folhasporparticipantes)
+
+if( multiplicacao >folhas) {
+    console.log("Não")
+
+} else {
+
+    console.log("Sim")
+}
+
+
+
+

ea75bbe5dc0b8266e3837e2a14d5b3f8980279f3
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-02 22:32:36 -0300

    usando a function

---
+++ dist/exercicio3.js
@@ -0,0 +1,9 @@
+var n1 = 3;
+var n2 = 7;
+var n3 = 5;
+//let media: number = (nota1 + nota2 + nota3) / 3
+//let fMedia = function(nota1:number, nota2:number, nota3:number) : number {
+//   return (nota1 + nota2 + nota3) / 3
+//}
+var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
+console.log("A média é " + fMedia(n1, n2, n3));
